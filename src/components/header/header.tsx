44c014ed74501d0890e8caf3a6f88ff92732d45b
robin3010 <robin3010@gmail.com>
2024-07-10 01:17:06 +0300

    app rename, update header title

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import './header.scss'
 const Header = () => {
   return (
     <header className="header">
-      <h3 className="header__app-title">To-do list calendar</h3>
+      <div className="header__app-title">CalenToDo</div>
       <div className="header__user-select">
         <span className="header__user-select-title">User select</span>
         <input className="header__user-select-input" />
